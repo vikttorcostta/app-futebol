ae702b3c966dd99675d5d86e9d07cc985e0c7e9a
viktt <victorfotografia12@gmail.com>
2024-08-25 22:12:50 -0300

    :sparkles: declarações dos componentes criados para app module

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,18 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
 import { CampeonatoBrasileiroSerieAComponent } from './campeonato-brasileiro-serie-a/campeonato-brasileiro-serie-a.component';
+import {TabelaComponent} from "./campeonato-brasileiro-serie-a/tabela/tabela.component";
+import {RodadasComponent} from "./campeonato-brasileiro-serie-a/rodadas/rodadas.component";
+import {FasesComponent} from "./campeonato-brasileiro-serie-a/fases/fases.component";
+import {ArtilhariaComponent} from "./campeonato-brasileiro-serie-a/artilharia/artilharia.component";
+import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 
 @NgModule({
   declarations: [
     AppComponent,
     DefaultLayoutComponent,
-    CampeonatoBrasileiroSerieAComponent
+    CampeonatoBrasileiroSerieAComponent,
+    TabelaComponent,
+    RodadasComponent,
+    FasesComponent,
+    ArtilhariaComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimationsAsync(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
