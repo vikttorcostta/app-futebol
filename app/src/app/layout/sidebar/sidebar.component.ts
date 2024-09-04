15cc234431b595196a3631ce5665fb6489e65409
Paulo Victor <paulo.v.melo@ba.estudante.senai.br>
2024-09-04 09:08:49 -0300

    :sparkles: Feat: Foi adicionados, novos icones e botões referentes a partidas, partidas ao vivo, rodadas, artilharia e fases

--- app/src/app/layout/sidebar/sidebar.component.ts
+++ app/src/app/layout/sidebar/sidebar.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  public logoPrincipal: string;
+
+  constructor(){
+
+    this.logoPrincipal = "/assets/scorebr.png";
+
+  }
+
 }
